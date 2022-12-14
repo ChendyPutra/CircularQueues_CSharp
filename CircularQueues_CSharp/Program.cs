63c5e6e536280b1d2fb3d0a53e6290a019c86abf
Chendy Lundy <chendy.lundy.ft21@mail.umy.ac.id>
2022-12-14 17:12:48 +0700

    Make a program circular queues with language csharp

---
+++ CircularQueues_CSharp/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularQueues_CSharp
+{
+    internal class Program
+    {
+        class Queues
+        {
+            int FRONT, REAR, max = 5;
+            int[] queue_array = new int[5];
+            public Queues()
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            public void insert(int element)
+            {
+                if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+                {
+                    Console.WriteLine("\nQueue overflow\n");
+                    return;
+                }
+                if (FRONT == -1)
+                {
+                    FRONT = 0;
+                    REAR = 0;
+                }
+                else
+                {
+                    if (REAR == max - 1)
+                        REAR = 0;
+                    else
+                        REAR = REAR + 1;
+                }
+                queue_array[REAR] = element;
+            }
+            public void remove()
+            {
+                if (FRONT == -1)
+                {
+                    Console.WriteLine("Queue underflow\n");
+                    return;
+                }
+                Console.WriteLine("\nThe element deleted from the queue is : " + queue_array[FRONT] + "\n");
+
+                if (FRONT == REAR)
+                {
+                    FRONT = -1;
+                    REAR = -1;
+                }
+                else
+                {
+                    if (FRONT == max - 1)
+                        FRONT = 0;
+                    else
+                        FRONT = FRONT + 1;
+                }
+            }
+            public void display()
+            {
+                int FRONT_position = FRONT;
+                int REAR_position = REAR;
+
+                if (FRONT == -1)
+                {
+                    Console.WriteLine("Queue is emppty\n");
+                    return;
+                }
+                Console.WriteLine("\nElements in the queue are...........\n");
+                if (FRONT_position <= REAR_position)
+                {
+                    while (FRONT_position <= REAR_position)
+                    {
+                        Console.Write(queue_array[FRONT_position] + " ");
+                        FRONT_position++;
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    while (FRONT_position <= max - 1)
+                    {
+                        Console.Write(queue_array[FRONT_position] + " ");
+                        FRONT_position++;
+                    }
+                    FRONT_position = 0;
+
+                    while (FRONT_position <= REAR_position)
+                    {
+                        Console.Write(queue_array[FRONT_position] + " ");
+                        FRONT_position++;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Queues q = new Queues();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1, Implement insert operation");
+                    Console.WriteLine("2. Implement insert operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice (1-4): ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch(ch)
+                    {
+                        case '1':
+                            {
+                                Console.Write("Enter a Number: ");
+                                int num = Convert.ToInt32(System.Console.ReadLine());
+                                Console.WriteLine();
+                                q.insert(num);
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.remove();
+                            }
+                            break;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                                return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid option!!");
+                            }
+                            break;
+                            
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered.");
+                }
+            }
+        }
+    }
+}
